4c9f5fa940396968c00cf4c6658cffbc798c2432
Bryan Hann <nobody@nowhere>
2025-12-07 17:37:23 -0330

    Major refactor.

--- root.d/dot-config/bch/activate.sh
+++ root.d/dot-config/bch/activate.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 [ -f ~/DEBUG ] && echo "[:dbg:] ++[$BASH_SOURCE]"
+
 export BCH_000_ROOT=$(dirname $BASH_SOURCE)
 export BCH_000_VEND=$(dirname $BASH_SOURCE)/vendor
-source $BCH_000_ROOT/init/fn.sh
-:lbin: $BCH_000_ROOT/lbin
+
 source $BCH_000_ROOT/init/init.sh
+
 [ -f ~/DEBUG ] && echo "[:dbg:] --[$BASH_SOURCE]"
--- root.d/dot-config/bch/init/init.sh
+++ root.d/dot-config/bch/init/init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+source $BCH_000_VEND/bash_colors.sh
+
 for pth in $(ls $(dirname ${BASH_SOURCE[0]})/[0-9]*); do
-    :dbg: ". $(basename $pth)"
-    source $pth
+    . $pth
 done
+
+
+:lbin: $BCH_000_ROOT/lbin
